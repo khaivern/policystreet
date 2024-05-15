8ed72d415a0e9dc68409209ba11292d6ef7c269b
khaivern <xarelata@gmail.com>
2024-05-15 20:39:40 +0800

    feat: add pagination

--- src/app/listing/listing.component.ts
+++ src/app/listing/listing.component.ts
@@ -1,17 +1,25 @@
 import { CommonModule } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
 import { MatIconModule } from "@angular/material/icon";
+import { MatPaginatorModule, PageEvent } from "@angular/material/paginator";
 import { MatTableModule } from "@angular/material/table";
 import { NgxSkeletonLoaderModule } from "ngx-skeleton-loader";
-import { BehaviorSubject } from "rxjs";
-import { SearchFilterComponent } from "../shared/components/search-filter/search-filter.component";
 import {
-    ICompany,
-    IFilter,
-    IPaginationMetadata,
+  BehaviorSubject,
+  Observable,
+  combineLatest,
+  switchMap,
+  tap,
+} from "rxjs";
+import { SearchFilterComponent } from "../shared/components/search-filter/search-filter.component";
+import { StatusFilterComponent } from "../shared/components/status-filter/status-filter.component";
+import {
+  ICompany,
+  ICompanyResponse,
+  IFilter,
+  IPaginationMetadata,
 } from "../shared/interfaces/common.interface";
 import { ListingService } from "../shared/services/listing.service";
-import { StatusFilterComponent } from '../shared/components/status-filter/status-filter.component';
 
 @Component({
   selector: "app-listing",
@@ -21,6 +29,7 @@ import { StatusFilterComponent } from '../shared/components/status-filter/status
     MatIconModule,
     NgxSkeletonLoaderModule,
     MatTableModule,
+    MatPaginatorModule,
     SearchFilterComponent,
     StatusFilterComponent,
   ],
@@ -63,29 +72,44 @@ export class ListingComponent implements OnInit {
   ngOnInit(): void {
     /* This is a dummy API used to simulate a GET request to obtain the relevant data
      * You are expected to start here and modify this code to use RxJs practices. Happy Coding! */
-    this.listingService
+    combineLatest([this.filterQuery$, this.pagination$])
+      .pipe(switchMap(() => this.getListing$()))
+      .subscribe();
+  }
+
+  public getRowIndex(index: number): number {
+    const pageIndex = this.pagination$.value.page - 1;
+    return pageIndex * this.pagination$.value.pageSize + index + 1;
+  }
+
+  public onSearch(keyword: string): void {
+    this.filterProperties.searchValue = keyword;
+    this.filterQuery$.next({ ...this.filterProperties });
+  }
+
+  public onStatusChange(statuses: string[]): void {
+    this.filterProperties.statusList = statuses;
+    this.filterQuery$.next({ ...this.filterProperties });
+  }
+
+  public onPaginate({ pageIndex, pageSize }: PageEvent): void {
+    this.paginationProperties.page = pageIndex + 1;
+    this.paginationProperties.pageSize = pageSize;
+    this.pagination$.next({ ...this.paginationProperties });
+  }
+
+  private getListing$(): Observable<ICompanyResponse> {
+    return this.listingService
       .getListing(
         this.paginationProperties.page,
         this.paginationProperties.pageSize,
         this.filterProperties,
       )
-      .subscribe((x) => {
-        this.data = x.data;
-      });
-  }
-
-  getRowIndex(index: number): number {
-    const pageIndex = this.pagination$.value.page - 1;
-    return (pageIndex * this.pagination$.value.pageSize) + index + 1;
-  }
-
-  onSearch(keyword: string): void {
-    const initialValue = this.filterQuery$.value;
-    this.filterQuery$.next({ ...initialValue, searchValue: keyword });
-  }
-
-  onStatusChange(statuses: string[]): void {
-    const initialValue = this.filterQuery$.value;
-    this.filterQuery$.next({ ...initialValue, statusList: statuses });
+      .pipe(
+        tap((result) => {
+          this.data = result.data;
+          this.paginationProperties = result.metadata;
+        }),
+      );
   }
 }
